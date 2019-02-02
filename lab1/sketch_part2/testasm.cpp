34c0c301e081a44fa2d05a15f72dd83aee4c8f3b
kshia <kristina.shia@yale.edu>
2019-02-01 16:06:38 -0800

    Cleaned up Part2

--- lab1/sketch_part2/testasm.cpp
+++ lab1/sketch_part2/testasm.cpp
@@ -31,29 +31,44 @@ start_of_assembly:
    ;       r24 = a, r25 = b
    ;
 
-   mov r22,r24
-   mov r24,r25
+   ; Setup arguments, left aligned on even-numbered registers
+   ; r24 = a, r22 = b
+   mov r22,r25
 
-   push r24
+   ; Push registers onto the stack right to left to save
+   ; Stack will look like this after calling subroutine:
+   ; | return address |
+   ; |   r24 = a      |
+   ; |   r22 = b      |
    push r22
-   
+   push r24
+
+   ; Call subroutine for sumval
    rcall sumval
-   
+
+   ; Save return values r25:r24 from sumval into r27:r26
    mov r26,r24
    mov r27,r25
 
-   pop r22
+   ; Restore registers containing original arguments
+   ; These will be the arguments for the next subroutine
+   ; Stack will look like this after calling subroutine:
+   ; | return address |
    pop r24
+   pop r22
 
+   ; Uses arguments, left aligned on even-numbered registers
    rcall diffval
 
+   ; Add result from diffval (r25:r24) to result of sumval (r27:r26)
+   ; Add lower byte then add with carry for higher byte
    add r24,r26
    adc r25,r27
 
    rjmp end_of_assembly
 
 sumval:
-   ; Add values in r22 and r24
+   ; Add values in r22 (b) and r24 (a)
    add r24,r22
    
    ; Branch if carry set
@@ -69,21 +84,25 @@ carry:
    ret
 
 diffval:
+  ; Clear r25 since there will be no carry
   ldi r25,0
-  ;cp r24,r22
-  cp r22,r24
-  brsh a_greater_eq
-  sub r24,r22
-  ret
 
-a_greater_eq:
+  ; Compare r24 (a) and r22 (b)
+  cp r24,r22
+
+  ; Branch if r24 (a) >= r22 (b)
+  brsh a_greater_eq
+
+  ; If a < b, r22 = b - a
+  ; Move result to r24 and return
   sub r22,r24
   mov r24,r22
   ret
 
-;b_greater_eq:
-;  sub r24,r22
-;  ret
+  ; Return a-b
+a_greater_eq:
+  sub r24,r22
+  ret
 
    ;
    ;  --- YOUR CODE ENDS ---
