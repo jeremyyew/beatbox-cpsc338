73092126dd735170cdc9fcc82a2dc4487da1fd73
jeremyyew <yewern@gmail.com>
2019-02-04 21:11:31 -0500

    Change part 2

--- lab1/sketch_part2/sketch_part2.ino
+++ lab1/sketch_part2/sketch_part2.ino
@@ -1,10 +1,15 @@
 #include "testasm.h"
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once
   Serial.begin(9600);
 
   // Test simple case
+  Serial.print("sumval(0,0)+diffval(0,0): ");
+  Serial.print(testasm(0, 0));
+  Serial.println(" (Expected: 0)");
+
   Serial.print("sumval(3,4)+diffval(3,4): ");
   Serial.print(testasm(3, 4));
   Serial.println(" (Expected: 8)");
@@ -19,13 +24,19 @@ void setup() {
   Serial.print(testasm(2, 255));
   Serial.println(" (Expected: 510)");
 
-  pinMode (13, OUTPUT);
+  // Test with neg
+  Serial.print("sumval(5, -1) + diffval(5, -1):");
+  Serial.print(testasm(5, -1));
+  Serial.println(" (Expected: 510) due to two's complement");
+
+  pinMode(13, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  digitalWrite (13, HIGH);
+  digitalWrite(13, HIGH);
   delay(400);
-  digitalWrite (13, LOW);
+  digitalWrite(13, LOW);
   delay(400);
 }
--- lab1/sketch_part2/testasm.cpp
+++ lab1/sketch_part2/testasm.cpp
@@ -1,13 +1,41 @@
 #include "testasm.h"
 
 /* the attribute "used" tells the compiler to not inline this function */
-__attribute__((used))
-unsigned int testasm(unsigned char a, unsigned char b) {
-  unsigned int out;
+__attribute__((used)) unsigned int testasm(unsigned char a, unsigned char b)
+{
+   unsigned int out;
 
-  asm volatile (R"(
+   asm volatile(R"(
 
    rjmp start_of_assembly
+
+sumval:
+   ; Add values in r22 (b) and r24 (a)
+   add r24,r22
+   clr r25
+   adc r25, r1
+   ret
+
+diffval:
+  ; Clear r25 since there will be no carry
+  clr r25
+
+  ; Compare r24 (a) and r22 (b)
+  cp r24,r22
+
+  ; Branch if r24 (a) >= r22 (b)
+  brsh a_greater_eq
+
+  ; If a < b, r22 = b - a
+  ; Move result to r24 and return
+  sub r22,r24
+  mov r24,r22
+  ret
+
+  ; Return a-b
+a_greater_eq:
+  sub r24,r22
+  ret
 
    .data
    ; any data values/static storage can go here
@@ -35,27 +63,20 @@ start_of_assembly:
    ; r24 = a, r22 = b
    mov r22,r25
 
-   ; Push registers onto the stack right to left to save
-   ; Stack will look like this after calling subroutine:
-   ; | return address |
-   ; |   r24 = a      |
-   ; |   r22 = b      |
-   push r22
-   push r24
+   ; Save input elsewhere
+   mov r18, r22
+   mov r19, r24
 
    ; Call subroutine for sumval
    rcall sumval
 
-   ; Save return values r25:r24 from sumval into r27:r26
+   ; Save return value r25:r24 from sumval into r27:r26
    mov r26,r24
    mov r27,r25
 
-   ; Restore registers containing original arguments
-   ; These will be the arguments for the next subroutine
-   ; Stack will look like this after calling subroutine:
-   ; | return address |
-   pop r24
-   pop r22
+   ; Restore input into r22, r24
+   mov r22, r18
+   mov r24, r19
 
    ; Uses arguments, left aligned on even-numbered registers
    rcall diffval
@@ -67,42 +88,6 @@ start_of_assembly:
 
    rjmp end_of_assembly
 
-sumval:
-   ; Add values in r22 (b) and r24 (a)
-   add r24,r22
-   
-   ; Branch if carry set
-   brcs carry
-   
-   ; Clear r25 if no carry
-   ldi r25,0
-   ret
-
-   ; Set r25 to 1 if carry
-carry:
-   ldi r25,1
-   ret
-
-diffval:
-  ; Clear r25 since there will be no carry
-  ldi r25,0
-
-  ; Compare r24 (a) and r22 (b)
-  cp r24,r22
-
-  ; Branch if r24 (a) >= r22 (b)
-  brsh a_greater_eq
-
-  ; If a < b, r22 = b - a
-  ; Move result to r24 and return
-  sub r22,r24
-  mov r24,r22
-  ret
-
-  ; Return a-b
-a_greater_eq:
-  sub r24,r22
-  ret
 
    ;
    ;  --- YOUR CODE ENDS ---
@@ -113,12 +98,13 @@ end_of_assembly:
    ; -- move r25:r24 to the 16-bit word in variable out
    movw %0,r24
 
-)" : "=w" (out)  /* out is %0, and a 16-bit variable that is modified
+)"
+                : "=w"(out)      /* out is %0, and a 16-bit variable that is modified
 		    by the assembly language */
-   : "r" (a), "r" (b)  /* a is %1, b is %2, both are register-sized
+                : "r"(a), "r"(b) /* a is %1, b is %2, both are register-sized
 			  variables */
-   : "r25", "r24");   /* r24, r25 are modified by this assembly
+                : "r25", "r24"); /* r24, r25 are modified by this assembly
 			 language fragment */
 
-  return out;
+   return out;
 }
