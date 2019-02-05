812fa711089d3a8e9cd6a924218ef6b55ebb11d8
jeremyyew <yewern@gmail.com>
2019-02-04 20:29:59 -0500

    Add test case

--- lab1/sketch_part1/sketch_part1.ino
+++ lab1/sketch_part1/sketch_part1.ino
@@ -1,6 +1,7 @@
 #include "testasm.h"
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once
   Serial.begin(9600);
 
@@ -8,24 +9,31 @@ void setup() {
   Serial.print("The sum of 3 and 4 is: ");
   Serial.print(testasm(3, 4));
   Serial.println(" (Expected 7)");
+  Serial.print("The sum of 4 and 3 is: ");
+  Serial.print(testasm(4, 3));
+  Serial.println(" (Expected 7)");
 
   // Test simple carry
   Serial.print("The sum of 255 and 1 is: ");
   Serial.print(testasm(255, 1));
+  Serial.println(" (Expected 256)");
+  Serial.print("The sum of 1 and 255 is: ");
+  Serial.print(testasm(1, 255));
   Serial.println(" (Expected 256)");
 
   // Test carry
   Serial.print("The sum of 255 and 255 is: ");
   Serial.print(testasm(255, 255));
   Serial.println(" (Expected 510)");
-  
-  pinMode (13, OUTPUT);
+
+  pinMode(13, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  digitalWrite (13, HIGH);
+  digitalWrite(13, HIGH);
   delay(400);
-  digitalWrite (13, LOW);
+  digitalWrite(13, LOW);
   delay(400);
 }
