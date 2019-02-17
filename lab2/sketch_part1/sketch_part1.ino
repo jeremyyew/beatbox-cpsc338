7ad5a91b6e08874a9ca413a02598f1ee06224da5
kshia <kristina.shia@yale.edu>
2019-02-17 13:17:45 -0500

    Part 1 with debouncing and comments

--- lab2/sketch_part1/sketch_part1.ino
+++ lab2/sketch_part1/sketch_part1.ino
@@ -1,22 +1,32 @@
-#include <avr/io.h>
+bool state;
+unsigned long time;
 
 void setup()
 {
-    PORTD |= (1 << PD2);
-    DDRD &= ~(1 << PD2);
-    // pinMode(4, INPUT_PULLUP);
-    Serial.begin(9600);
+  // Set up PD2 for input with passive pull-up
+  PORTD |= (1 << PD2);
+  DDRD &= ~(1 << PD2);
+  
+  state = false;
+  time = millis();
+  Serial.begin(9600);
 }
 
 void loop()
 {
-    // int buttonValue = digitalRead(buttonPin);
-    if ((PIND & (1 << PD2)) == 4)
-    {
-        Serial.print("ON");
+  // Set pinState to the value of PD2
+  bool pinState = (PIND & (1 << PD2));
+
+  // Debounce by checking if 200 ms has passed since last state change
+  if (millis() > time + 200 && pinState != state) {
+    if (pinState) {
+        Serial.println("Release (OFF)");
+    } else {
+        Serial.println("Press (ON)");
     }
-    else
-    {
-        Serial.print("OFF");
-    }
+    state = pinState;
+
+    // Reset timer after state change
+    time = millis();
+  }
 }
