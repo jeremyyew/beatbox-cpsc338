a0d0d1f5587b6e139fe74e88021b86f09829c363
jeremyyew <yewern@gmail.com>
2019-02-18 02:33:39 +0000

    Update schematics

--- lab2/sketch_part2/sketch_part2.ino
+++ lab2/sketch_part2/sketch_part2.ino
@@ -2,20 +2,22 @@ int latchPin = 12;
 int clockPin = 11;
 int dataPin = 13;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-    pinMode(latchPin, OUTPUT);
-    pinMode(dataPin, OUTPUT);
-    pinMode(clockPin, OUTPUT);
-    Serial.begin(9600);
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
-   digitalWrite(latchPin, LOW);
-   shiftOut(dataPin, clockPin, LSBFIRST, B00001010); 
-   digitalWrite(latchPin, HIGH);
-   Serial.print("hi");
-   delay(250);
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, B00001010);
+  digitalWrite(latchPin, HIGH);
+  Serial.print("hi");
+  delay(250);
 }
