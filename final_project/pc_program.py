2a8ce5f85c5071b20bf87722edb29a76faa2fd44
kshia <kristina.shia@yale.edu>
2019-04-23 22:30:54 -0400

    nice button numbers

--- final_project/pc_program.py
+++ final_project/pc_program.py
@@ -1,17 +1,21 @@
 from time import sleep
 import serial
 
-ser = serial.Serial('/dev/cu.usbmodem141201', 9600, timeout=0)
+ser = serial.Serial('/dev/cu.usbmodem1421', 115200, timeout=0)
+
+def button_pressed(button_num):
+   print("pressed:", button_num)
+
+def button_released(button_num):
+   print("released:", button_num)
 
 received = False
 while not received:
-   print("p1 sending")
-   ser.write("hello?\r".encode()) # Convert the decimal number to ASCII then send it to the Arduino
    if ser.inWaiting() > 0: 
-      incoming = ser.readline().decode()
-      print("incoming:"+incoming)
-      print(ascii(incoming))
-      if incoming == "hey!\r\n":
-         received = True
-   sleep(1) # Delay for one tenth of a second
-print("received") # Read the newest output from the Arduino
+      incoming = ser.read()
+      action_num = ord(incoming) - 48
+      if (action_num < 16):
+         button_pressed(action_num)
+      else:
+         button_released(action_num - 16)
+
