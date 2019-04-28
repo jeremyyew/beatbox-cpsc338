08261310582dd4d44514ff28dec46c22fa181299
jeremyyew <yewern@gmail.com>
2019-04-28 02:24:11 -0400

    Added separate prompts file

--- final_project/single_threaded.py
+++ final_project/single_threaded.py
@@ -1,41 +1,45 @@
-import pygame, time, threading
+import pygame
+import time
+import threading
 import serial
 from enum import Enum
 from collections import OrderedDict
+from prompts import get_prompts
 
+# TODO:
+# Sat Night:
+# -x circuit diagram
+# - poster
+
+# Sun:
+# - transcribe a song
+# - second song/game state?
+# - enclosure
+# - print poster
+PORT = '/dev/cu.usbmodem1411'
 music = pygame.mixer.music
 
-firefly_prompts = {
-    # beat number : key buttons that should be pressed within beat
-    # 1: [1],
-    # 2: [2, 3],            # can't have holds since the user wouldn't know to press twice?
-    # 3: [3, 4, 8, 9],
-    # 4: [4],
-    # 8: [1]
-    2: [1],
-    4: [2],
-    6: [1],
-    8: [2],
-    10: [1],
-    12: [2]
+firefly_prompts = get_prompts()
 
-}
 
 class Color(Enum):
-   OFF = 0
-   A = 1
-   B = 2
-   C = 3
+    OFF = 0
+    A = 1
+    B = 2
+    C = 3
 
 
 def set_color(button_num, color):
     button_states[button_num] = color
-    print("lighting up:", button_num, color, button_num + 48 + 16 * color.value)
+    print("lighting up:", button_num, color,
+          button_num + 48 + 16 * color.value)
     ser.write(bytes(chr(button_num + 48 + 16 * color.value), 'utf-8'))
+
 
 def turn_off(button_num):
     print("turning off:", button_num)
     ser.write(bytes(chr(button_num + 48 + 16 * Color.OFF.value), 'utf-8'))
+
 
 class Game:
     def __init__(self, title, tempo, prompts, duration_beats):
@@ -65,12 +69,12 @@ class Game:
                     if current_beat not in self.prompts or button_num not in self.prompts[current_beat]:
                         turn_off(button_num)
 
-                self.last_updated = current_beat                
-                
-                # TODO: Check if there were any prompts that were not triggered. 
+                self.last_updated = current_beat
+
+                # TODO: Check if there were any prompts that were not triggered.
 
                 if current_beat in self.prompts:
-                    # Update. 
+                    # Update.
                     self.active_prompts = set(self.prompts[current_beat])
                     for button_num in self.prompts[current_beat]:
                         set_color(button_num, Color.C)
@@ -80,7 +84,7 @@ class Game:
                     for button_num in self.prompts[next_beat]:
                         set_color(button_num, Color.A)
 
-            if ser.inWaiting() > 0: 
+            if ser.inWaiting() > 0:
                 incoming = ser.read()
                 action_num = ord(incoming) - 48
                 if (action_num < 16):
@@ -90,20 +94,19 @@ class Game:
 
         print("CORRECT:", self.correct)
         print("MISSED:", self.missed)
-        print("FINAL SCORE:",self.correct - self.missed)
+        print("FINAL SCORE:", self.correct - self.missed)
 
     def button_pressed(self, button_num):
         print(f'pressed {button_num}')
         new_color = Color.A             # Red if you press and you weren't supposed to
         if button_num in self.active_prompts:
             print(f'prompt {button_num} triggered')
-            # Remove prompt so we can't trigger it again. 
+            # Remove prompt so we can't trigger it again.
             self.active_prompts.remove(button_num)
             # Record score.
             self.correct += 1
 
             new_color = Color.B          # Green when pressed after prompt
-            
 
         set_color(button_num, new_color)
         print(f'handled {button_num} pressed')
@@ -112,14 +115,15 @@ class Game:
         print("released:", button_num)
 
 
-ser = serial.Serial('/dev/cu.usbmodem1411', 115200, timeout=0)
+ser = serial.Serial(PORT, 115200, timeout=0)
 print('Initializing...')
 buttons = range(0, 16)
 button_states = {key: Color.OFF for key in buttons}
-time.sleep(3) # allow time to initalize serial (restarts sketch)
+time.sleep(3)  # allow time to initalize serial (restarts sketch)
 
 # performance = {beat: [] for beat in firefly_prompts.keys()}
 
-game = Game(title='sounds/fireflies.mp3',tempo=90,prompts=firefly_prompts,duration_beats = 15)
-print(firefly_prompts)
+game = Game(title='sounds/fireflies.mp3', tempo=90,
+            prompts=prompts.firefly_prompts, duration_beats=45)
+print(prompts.firefly_prompts)
 game.start()
